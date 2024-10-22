78d4c2a6b4c6686d3b775f3e917ad32dd33dff4a
NgoMinhDuy0408 <duymicrosoft@gmail.com>
2024-10-23 00:32:22 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 2/Duy110186.java
@@ -0,0 +1,8 @@
+package buoi2;
+
+public class Duy110186 {
+    public static void main(String[] args) {
+        System.out.println("Tên sinh viên: Ngô Minh Duy");
+        System.out.println("MSSV: 110186");
+    }
+}
