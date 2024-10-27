d8035cd03e2c2ea6b55f1ac5a5cac1aa2ad5317b
NgoMinhDuy0408 <duymicrosoft@gmail.com>
2024-10-27 14:04:00 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 3/Duy110186.java
@@ -0,0 +1,18 @@
+package buoi3;
+public class Duy110186 {
+    public static void main(String[] args) {
+        String maSinhVien = "110186" , hoVaTen = "Ngô Minh Duy" , ngaySinh = "04/08/2006" , lop = "GD24A" , khoa = "Mỹ thuật ứng dụng" , diaChi = "Đà Nẵng" ;
+       int tuoi = 18;
+        boolean gioiTinh = true;
+        System.out.println("-----THÔNG TIN SINH VIÊN-----"); 
+        System.out.println("Mã sinh viên: " + maSinhVien);
+        System.out.println("Họ và tên: " + hoVaTen);
+        System.out.println("Ngày sinh: " + ngaySinh);
+        System.out.println("Lớp: " + lop);
+        System.out.println("Khoa: " + khoa);
+        System.out.println("Địa chỉ: " + diaChi);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("-----HẾT-----");
+    }
+    
+}
