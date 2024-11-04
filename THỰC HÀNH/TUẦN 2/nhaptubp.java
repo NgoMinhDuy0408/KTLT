e101b7eae6e38a1f3494c2ce383fad6e9029b86a
NgoMinhDuy0408 <duymicrosoft@gmail.com>
2024-11-05 00:12:40 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 2/nhaptubp.java
@@ -0,0 +1,14 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class nhaptubp {
+    public static void main(String[] args) {
+    String tenLop = "GD24A";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("xin chao cac ban tan sinh vien \nlop " + tenLop);
+        System.out.println("STT  HO VA TEN  LOP");
+        scanner.nextLine();
+    }
+    
+}
